05ef2f0032e0918059cf66b0010f63df7bc45031
Nathan Perry <nbp8557@rit.edu>
2015-10-16 13:45:10 -0400

    Added constructor to the TeamLead class

---
+++ TeamLead.java
@@ -0,0 +1,10 @@
+public class TeamLead{
+	public string name;
+	//Will have a collection of developers (in our scenario only 3)
+	
+	public TeamLead(string name){
+		this.name = name;		
+	}
+	
+	
+}
