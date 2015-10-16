5777c4708367ce57aa1284503fe1b5012d9a8292
Nathan Perry <nbp8557@rit.edu>
2015-10-16 13:42:02 -0400

    Psuedocoded the main method. Created a TeamLead class

--- Main.java
+++ Main.java
@@ -2,5 +2,19 @@ public class Main{
 	
 	public static void main(String[] args) {
 		//This will start the application
+		
+		//allocate all of the employee, manager objects
+		
+		//Start manager at 8 am
+		
+		//Utilize a latch a start the employees all together starting at
+		//what we consider 8am
+		
+		
+		// Kick off project status meeting at 4pm
+		
+		//Wait until after all employees have left the office, then exit the program
+		
+		
 	}
 }
