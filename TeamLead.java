3ae215dfbdc80ebae7d287cfdecea0abe89b8250
Michael Dirmyer <dir1993@yahoo.com>
2015-10-21 13:14:54 -0400

    added list of employees to teamlead

--- TeamLead.java
+++ TeamLead.java
@@ -1,11 +1,12 @@
 
 public class TeamLead extends Employee{
 	public String name;
+	public Employee[] teamMembers;
+
 	//Will have a collection of developers (in our scenario only 3)
 	
-	public TeamLead(String name){
-		this.name = name;		
+	public TeamLead(String name, Employee[] members){
+		this.name = name;
+		this.teamMembers = members;
 	}
-	
-	
 }
