3472a612d43122fdcdcaf952d5121ac1f50f1b6f
LukeHarrold <lvh3851@g.rit.edu>
2015-10-25 16:36:21 -0400

    teamlead logging

--- TeamLead.java
+++ TeamLead.java
@@ -11,7 +11,7 @@ public class TeamLead extends Employee{
 	private Clock clock;
 	private Manager manager;
 	private PriorityBlockingQueue<Employee> questions = new PriorityBlockingQueue<Employee>();
-	
+	private int lunch_time = 0, meeting_time = 0, question_time = 0, work_time = 0;
 	
 	public TeamLead(String name, int teamNum, Clock clck, Manager man){
 		super(temp, teamNum, 1, clck);
@@ -143,6 +143,7 @@ public class TeamLead extends Employee{
 			} else if(currentTime >= Clock.LUNCH && lunchTime == -1){
 				System.out.println(Clock.getTimeStr(currentTime) + " " + name + " went to lunch.");
 				lunchTime = super.lunch();
+				lunch_time  = clock.getCurrentTime() - currentTime;
 				try{
 					sleep(Clock.toRealtime(lunchTime));
 				} catch (InterruptedException e){}
@@ -150,13 +151,16 @@ public class TeamLead extends Employee{
 			}
 			else if(currentTime > clock.STANDUP){
 				TeamMorningStandup();
+				meeting_time += clock.getCurrentTime() - currentTime;
 			}
 			
 			else{
 				AnswerQuestion();
+				question_time += clock.getCurrentTime() - currentTime;
 			}
 		}
 		System.out.println(Clock.getTimeStr(clock.getCurrentTime()) + " " + name + " went home.");
+		work_time = 4800 - question_time - meeting_time - lunch_time;
 	}
 	public boolean AddEmployee(Employee dev) {
 		return this.teamMembers.add(dev);		
